93a09f8177a2254316fa2e631f1dff12a1fdd914
Patrick Podest <p.podest@enlite.ai>
2024-07-02 10:37:01 +0200

    Add vem-tabline keybindings

--- lua/custom/plugins/vem-tabline.lua
+++ lua/custom/plugins/vem-tabline.lua
@@ -1,3 +1,8 @@
 return {
   'pacha/vem-tabline',
+  config = function()
+    vim.keymap.set('n', '<leader>vn', '<Plug>vem_next_buffer-')
+    vim.keymap.set('n', '<leader>vp', '<Plug>vem_prev_buffer-')
+    vim.keymap.set('n', '<leader>vx', '<Plug>vem_delete_buffer-')
+  end,
 }
