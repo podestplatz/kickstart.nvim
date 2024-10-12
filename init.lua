0acc874fa908dba2e1a7a256f54e3b5e5fc828a2
Patrick Podest <patrick@podest.co.at>
2024-10-12 07:15:36 +0200

    set textwidth

--- init.lua
+++ init.lua
@@ -125,6 +125,11 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Settings for the default textwidth enabling gqip
+vim.opt.textwidth = 120
+vim.opt.formatoptions:append('t')
+vim.opt.formatprg = "par -w120"
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
