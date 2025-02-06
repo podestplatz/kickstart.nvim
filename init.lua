bf717c4ea040f140958abfa0ca9f958c8ab39d60
Patrick Podest <podest@ml.jku.at>
2025-02-06 08:29:12 +0100

    fix which-key

--- init.lua
+++ init.lua
@@ -379,12 +379,20 @@ require('lazy').setup({
       },
 
       -- Document existing key chains
-      spec = {
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      },
-    },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument'},
+        { '<leader>r', group = '[R]ename'},
+        { '<leader>s', group = '[S]earch'},
+        { '<leader>w', group = '[W]orkspace'},
+        { '<leader>t', group = '[T]oggle'},
+        { '<leader>h', group = 'Git [H]unk'},
+      }
+      -- visual mode
+      require('which-key').add({
+        {'<leader>h', group = 'Git [H]unk' },
+      }, { mode = 'v' })
+    end,
   },
 
   -- NOTE: Plugins can specify dependencies.
