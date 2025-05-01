e7911965d8269848c98b6d5eff2278afe93dd2ed
Patrick Podest <patrick@podest.co.at>
2025-05-01 07:11:27 +0200

    use hop also in visual mode

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -4,22 +4,26 @@ return {
   opts = {
     keys = 'etovxqpdygfblzhckisuran',
     quit_key = '<SPC>',
+    jump_on_sole_occurence = true,
   },
   keys = {
     {
       '<leader>jj',
       '<cmd>HopChar1<cr>',
       desc = 'Hop to a Character',
+      mode = { 'n', 'v' },
     },
     {
       '<leader>jl',
       '<cmd>HopLine<cr>',
       desc = 'Hop to [L]ine',
+      mode = { 'n', 'v' },
     },
     {
       '<leader>jw',
       '<cmd>HopWordCurrentLine<cr>',
       desc = 'Hop to [W]ord',
+      mode = { 'n', 'v' },
     },
   },
 }
