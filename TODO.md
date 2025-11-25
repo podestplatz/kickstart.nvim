3c2986418eb1c93a81e61a7ccfb1a0bd2df32bf6
Patrick Podest <podest@ml.jku.at>
2025-11-25 14:21:40 +0100

    adapt vem tabline

---
+++ TODO.md
@@ -0,0 +1,4 @@
+- add debugging
+- `<leader>or` shall search for and open a readme that is found in the repo. Therefore it shall only check the git 
+  tracked files. If multiple are found, a list shall be opened from which the right one can be selected. 
+
--- lua/custom/plugins/vem-tabline.lua
+++ lua/custom/plugins/vem-tabline.lua
@@ -1,8 +1,11 @@
 return {
   'pacha/vem-tabline',
+  dependencies = {
+    'ryanoasis/vim-devicons',
+  },
   config = function()
-    vim.keymap.set('n', '<leader>vn', '<Plug>vem_next_buffer-')
-    vim.keymap.set('n', '<leader>vp', '<Plug>vem_prev_buffer-')
+    vim.keymap.set('n', 'gt', '<Plug>vem_next_buffer-')
+    vim.keymap.set('n', 'gT', '<Plug>vem_prev_buffer-')
     vim.keymap.set('n', '<leader>vx', '<Plug>vem_delete_buffer-')
   end,
 }
